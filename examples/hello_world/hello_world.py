7e3a54c4752884606203e21ca093c2847fa6f289
Jacques Beaurain <jacques.beaurain@geosoft.com>
2016-10-18 14:20:53 -0400

    Reformatting comments

--- examples/hello_world/hello_world.py
+++ examples/hello_world/hello_world.py
@@ -1,6 +1,9 @@
-import geosoft.gxapi as gxapi	# gxapi methods
-import geosoft.gxpy as gxpy		# gxpy methods
+import geosoft.gxapi as gxapi # gxapi methods
+import geosoft.gxpy as gxpy   # gxpy methods
 
-def rungx():																# a python script must have a rungx(), which is executed by OM when the script is run
-    gxp = gxpy.gx.GXpy()													# get the current gx context
-    gxapi.GXSYS.display_message("GX Python", "Hello {}".format(gxp.gid))	# say hello to the user identified by gxp.gid.
+# a python script must have a rungx(), which is executed by OM when the script is run
+def rungx():
+    gxp = gxpy.gx.GXpy() # get the current gx context
+    
+    # say hello to the user identified by gxp.gid.
+    gxapi.GXSYS.display_message("GX Python", "Hello {}".format(gxp.gid))
