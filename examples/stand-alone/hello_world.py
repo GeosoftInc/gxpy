8fd7e6fc11d2e27ede8b4ab62d78e8f76d39f27b
IanMacLeod <ian.macleod@geosoft.com>
2016-10-27 06:19:43 -0400

    Simplified stand-along "Hello world" to support both stand-alone and extension

--- examples/stand-alone/hello_world.py
+++ examples/stand-alone/hello_world.py
@@ -1,11 +1,24 @@
 # This sample stand-alone Python script shows a minimal use of the Pythonic gxpy module to
 # create a Geosoft context and say hello to the user.
 
+# comment-out following 2 lines to support remote debugging
+#import pydevd
+#pydevd.settrace('localhost', port=34765, stdoutToServer=True, stderrToServer=True)
+
 import geosoft.gxpy as gxpy   # gxpy methods
 
-# Stand-alone programs must create a GX context before calling any Geosoft methods.
-gxcontext = gxpy.gx.GXpy()
+# running as an extension from Oasis montaj will execute rungx()
+def rungx():
 
-# The context has a member 'gid' which contains the user's Geosoft ID.
-# Say hello to the user
-print("Hello {}!".format(gxcontext.gid))
+    # say hello to the user identified by gxp.gid.
+    gxapi.GXSYS.display_message("GX Python", "Hello world")
+
+# running as stand-alone program
+if __name__ == "__main__":
+
+    # Stand-alone programs must create a GX context before calling any Geosoft methods.
+    gxc = gxpy.gx.GXpy()
+
+    # The context has a member 'gid' which contains the user's Geosoft ID.
+    # Say hello to the user
+    print("Hello world")
