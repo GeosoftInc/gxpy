58ba27a46b5447c39e85b99e4cdbe787bdaae6dd
Jacques Beaurain <jacques.beaurain@geosoft.com>
2016-11-09 13:30:53 -0500

    Fix and simplify gxpy.utility.check_version docstring

--- geosoft/gxpy/utility.py
+++ geosoft/gxpy/utility.py
@@ -38,28 +38,22 @@ def _(s):
 def check_version(v, raise_on_fail=True):
     """
     Check the minimum API version.
+
     :param v:               minimum version string required (ie "9.1" or "9.2.4")
     :param raise_on_fail:   if True, raises an error if the version check fails, returns False otherwise
     :return:                True if version is OK, False otherwise(unless raise_on_fail is False)
 
     .. note::  
-        A valid version consists of two or three dot-separated numeric components, with an optional "pre-release" tag
-        on the end.  The pre-release tag consists of the letter 'a' or 'b' followed by a number.  If the numeric components 
-        of two version numbers are equal, then one with a pre-release tag will always be deemed earlier (lesser) than one without.
+        A valid version consists of two or three dot-separated numeric components, with an optional development version tag
+        on the end.  The development version tag consists of the letter 'b' (for beta) followed by a number.  If the numeric components 
+        of two version numbers are equal, then a development version will always be deemed earlier (lesser) than one without.
 
-        The following are valid version numbers (shown in the order that
-        would be obtained by sorting according to the supplied cmp function):
+        The following are valid version numbers (shown in order of used for meeting minimum requirements):
 
-            * 0.4 or 0.4.0  (they are equivalent)
-            * 0.4.1
-            * 0.5a1
-            * 0.5b3
-            * 0.5
-            * 0.9.6
-            * 1.0
-            * 1.0.4a3
-            * 1.0.4b1
-            * 1.0.4
+            * 9.1 or 9.1.0  (they are equivalent)
+            * 9.1.1b0 (development version)
+            * 9.1.1
+            * 9.2
 
         The following are examples of invalid version numbers:
 
