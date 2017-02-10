22ae91aedfab0e5235fbf3df78a6a300ba6e83f4
Jacques Beaurain <jacques.beaurain@geosoft.com>
2017-02-10 08:58:52 -0500

    Fill in some blanks with the extraction of API specification files.
    Submitting last change in this repository. These files will move to
    gxapi repo now.

--- docs/transform/generate_new_api_spec.py
+++ docs/transform/generate_new_api_spec.py
@@ -1,26 +1,229 @@
 import os
 import re
+import textwrap
+from jinja2 import Environment, FileSystemLoader
+
+import gxgenutils
+
+global global_collection
+global_collection = gxgenutils.object_from_pickled_file('pyxb_all.pickled')
+
+def gen_replacements():
+    replace_dict = {}
+    for key, _ in global_collection.known_classes.items():
+        replace_dict[key] = ":class:`{}`".format(key)
+    for key, _ in global_collection.known_class_handles.items():
+        replace_dict[key] = ":class:`{}`".format(key)
+    for key, _ in global_collection.known_definitions.items():
+        replace_dict[key] = ":def:`{}`".format(key)
+    for key, _ in global_collection.known_methods.items():
+        replace_dict[key] = ":func:`{}`".format(key)
+    for key, _ in global_collection.known_definition_values.items():
+        replace_dict[key] = ":def_val:`{}`".format(key)
 
 
-from gxgenutils import *
+    # Sort by descending length to ensure correct replacement order with substring
+    keys = list(replace_dict.keys())
+    keys.sort(key=lambda x: len(x), reverse=True)
+    pattern = re.compile(r'\b(' + '|'.join(keys) + r')\b')
+    return pattern, replace_dict
+
+repl_pat, repl_dict = gen_replacements()
+
+def fix_doc_refs(text):
+    text = text.replace('</define>', '')
+    text = text.replace('<define>', '')
+    text = text.replace('.GXH', '')
+    text = repl_pat.sub(lambda x: repl_dict[re.escape(x.group())], text)
+    return text.strip()
 
 
-def generate_api_as_yml(j2env, dir):
-    pass
-
-def generate():
-    from jinja2 import Environment, FileSystemLoader
-    global __global_collection
-
-    dir = os.path.split(__file__)[0]
-    templates_dir = os.path.join(dir,'templates')
-    j2env = Environment(loader=FileSystemLoader(templates_dir), 
-						trim_blocks = True,
-						lstrip_blocks = True)
+j2env = None
+local_dir = os.path.split(__file__)[0]
+templates_dir = os.path.join(local_dir, 'gxapi_templates')
+j2env = Environment(loader=FileSystemLoader(templates_dir),
+                    trim_blocks=True,
+                    lstrip_blocks=True)
+do_indent = j2env.filters['indent']
 
 
-    generate_api_as_yml(j2env, dir)
+def clean_quote(value):
+    value = value.strip()
+    if '\n' in value:
+        return '"""\n{}\n"""'.format(value)
+    elif not '"' in value:
+        return '"{}"'.format(value)
+    elif not "'" in value:
+        return "'{}'".format(value)
+    else:
+        return '"{}"'.format(value.replace('"', r'\"'))
+
+
+def clean_doc(value, indent=0):
+
+    #if value == 'real':
+    #    return 'double'
+    #if value == 'int':
+    #    return 'int32_t'
+    doc = textwrap.dedent(value)
+    doc = fix_doc_refs(doc)
+
+    # replace all standalone \ characters with double escapes \\
+    doc = re.sub(r'(?<!\\)(\\[^\\])', r'\\\1', doc)
+
+    # replace all end of line standalone \ characters with a double escapes \\
+    doc = re.sub(r'(?<!\\)(\\$)', r'\\\1', doc)
+
+    if '\n' in doc and indent > 0:
+        doc = do_indent('{}\n"""'.format(doc), indent, True)
+        return '"""\n{}'.format(doc)
+    else:
+        return clean_quote(doc)
+
+
+j2env.filters['clean_doc'] = clean_doc
+j2env.filters['clean_quote'] = clean_quote
+
+core_set = set([
+    '3DN',
+    '3DV',
+    'AGG',
+    'BF',
+    'DAT',
+    'DATALINKD',
+    'DATAMINE',
+    'DB',
+    'DBREAD',
+    'DBWRITE',
+    'DSEL',
+    'EXT',
+    'GEO',
+    'GEOSOFT',
+    'GEOSTRING',
+    'GIS',
+    'HGD',
+    'HXYZ',
+    'IGRF',
+    'IMG',
+    'IMU',
+    'IPJ',
+    'ITR',
+    'LAYOUT',
+    'LL2',
+    'LPT',
+    'LST',
+    'LTB',
+    'MAP',
+    'MAPL',
+    'MAPTEMPLATE',
+    'MATH',
+    'META',
+    'MVIEW',
+    'MVU',
+    'MXD',
+    'PAT',
+    'PG',
+    'PJ',
+    'PLY',
+    'RA',
+    'REG',
+    'SBF',
+    'ST',
+    'ST2',
+    'STR',
+    'SURFACE',
+    'SURFACEITEM',
+    'SYS',
+    'TB',
+    'TPAT',
+    'TR',
+    'USERMETA',
+    'VA',
+    'VM',
+    'VOX',
+    'VOXD',
+    'VOXE',
+    'VULCAN',
+    'VV',
+    'WA'
+])
+
+desk_set = set([
+    'ACQUIRE',
+    'ARCDB',
+    'ARCDH',
+    'ARCMAP',
+    'ARCSYS',
+    'BIGRID',
+    'CHIMERA',
+    'COM',
+    'CSYMB',
+    'DGW',
+    'DH',
+    'DMPPLY',
+    'DOCU',
+    'DU',
+    'DXFI',
+    'EDB',
+    'EDOC',
+    'EMAP',
+    'EMAPTEMPLATE',
+    'EUL3',
+    'EXP',
+    'FFT',
+    'FFT2',
+    'FLT',
+    'GD',
+    'GER',
+    'GMSYS',
+    'GU',
+    'GUI',
+    'HTTP',
+    'IEXP',
+    'INTERNET',
+    'IP',
+    'IPGUI',
+    'KGRD',
+    'LMSG',
+    'MISC',
+    'MSTK',
+    'MVG',
+    'PDF3D',
+    'PGEXP',
+    'PGU',
+    'PRAGA3',
+    'PROJ',
+    'RGRD',
+    'SEMPLOT',
+    'SHP',
+    'SQLSRV',
+    'STK',
+    'STRINGS',
+    'TC',
+    'TEST',
+    'TIN',
+    'TRND',
+    'UNC',
+    'VAU',
+    'VVEXP',
+    'VVU'
+])
+
+def generate_class_as_rst(cl, template):
+    if cl.name in core_set:
+        branch = 'core'
+    else:
+        branch = 'desk'
+    with open('../../../gxapi/spec/' + branch + '/' + cl.name + '.py', 'w+') as f:
+        f.write(template.render(cl=cl))
+
+def generate_spec():
+    template = j2env.get_template('api_spec.pyj')
+    for _, cl in global_collection.known_classes.items():
+        generate_class_as_rst(cl, template)
+
 
 if __name__ == "__main__":
-    global_collection = object_from_pickled_file('pyxb_all.pickled')
-    generate()
+    generate_spec()
+
+
