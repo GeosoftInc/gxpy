6d703f69374d98b116d65d36ac0e35aebf25773e
IanMacLeod <ian.macleod@geosoft.com>
2017-06-15 10:03:07 -0400

    Added tutorial exercise files and sample datasets

--- tutorial/Working with grids/grid_convert_format.py
+++ tutorial/Working with grids/grid_convert_format.py
@@ -5,9 +5,18 @@ import geosoft.gxpy.grid as gxgrid
 gxc = gx.GXpy()
 
 # open grid
+<<<<<<< HEAD
 grid_surfer = gxgrid.Grid.open('elevation_surfer.grd(SRF;VER=V7)')
 
 # copy the grid to an ER Mapper format grid file
 grid_erm = gxgrid.Grid.copy(grid_surfer, 'elevation.ers(ERM)', overwrite=True)
 
 exit()
+=======
+grid_geosoft = gxgrid.open('elevation.grd(GRD)')
+
+# copy the grid to a Grid eXchange File
+grid_gxf = gxgrid.copy(grid_geosoft, 'elevation.gxf(GXF)', overwrite=True)
+
+pass
+>>>>>>> Added tutorial exercise files and sample datasets
