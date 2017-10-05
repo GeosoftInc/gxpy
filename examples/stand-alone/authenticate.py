a76238aa8da5960c13b53407fdb06c52e49d878c
Ian MacLeod <ian.macleod@geosoft.com>
2017-10-05 17:01:33 -0400

    authenticate test app

--- examples/stand-alone/authenticate.py
+++ examples/stand-alone/authenticate.py
@@ -1,3 +1,11 @@
-import geosoft.gxpy as gxpy
-gxc = gxpy.gx.GXpy()
-print('authenticated:', gxc.gid)
+import geosoft.gxapi as gxapi
+ctx = gxapi.GXContext.create('ian', '0.0', 0, 0)
+user = gxapi.str_ref()
+company = gxapi.str_ref()
+gxapi.GXSYS.get_licensed_user(user, company)
+
+try:
+    gxapi.GXST2.create()
+    print(user.value, 'is licenced.')
+except gxapi.GXAPIError:
+    print(user.value, 'is NOT licenced.')
