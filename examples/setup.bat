aa5514fbd10945e21728d49f707e39c4126b4d9e
Ian MacLeod <ian.macleod@geosoft.com>
2017-10-27 13:35:11 -0400

    Added example menu to run sample extensions

---
+++ examples/setup.bat
@@ -0,0 +1,6 @@
+md "C:\Program Files\Geosoft\Desktop Applications 9\user\omn"
+copy pyexamples.omn "C:\Program Files\Geosoft\Desktop Applications 9\user\omn"
+
+md "C:\Program Files\Geosoft\Desktop Applications 9\user\python"
+copy om-extensions\*.py "C:\Program Files\Geosoft\Desktop Applications 9\user\python"
+copy geosoft_research\self_organizing_maps\som_om.py "C:\Program Files\Geosoft\Desktop Applications 9\user\python"
