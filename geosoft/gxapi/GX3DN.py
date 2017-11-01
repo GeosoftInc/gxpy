6dae1dba8b44a392db47c3d1aad2b9ebba067387
Jacques Beaurain <jacques.beaurain@geosoft.com>
2017-11-01 14:33:59 -0400

    Remove block test code

--- geosoft/gxapi/GX3DN.py
+++ geosoft/gxapi/GX3DN.py
@@ -351,8 +351,6 @@ class GX3DN:
 ### endblock ClassImplementation
 ### block ClassExtend
 # NOTICE: The code generator will not replace the code in this block
-    def test(self):
-        print("test")
 ### endblock ClassExtend
 
 
