978964ff3175a41b86bb0244b65927a20a7d68d4
IanMacLeod <ian.macleod@geosoft.com>
2017-11-03 16:17:44 -0400

    3D Tutorial

--- tutorial/3D Views/tmi_as_3d_relief.py
+++ tutorial/3D Views/tmi_as_3d_relief.py
@@ -1,5 +1,4 @@
 import geosoft.gxpy.gx as gx
-import geosoft.gxpy.map as gxmap
 import geosoft.gxpy.view as gxview
 import geosoft.gxpy.group as gxgroup
 import geosoft.gxpy.agg as gxagg
@@ -11,17 +10,22 @@ gxc = gx.GXpy()
 grid_file = 'Wittichica Creek Residual Total Field.grd'
 
 # create a 3D view
-with gxview.View_3d.new("data",
-                        area_2d=gxgrd.Grid(grid_file).extent_2d(),
+with gxview.View_3d.new("TMI in relief",
+                        area_2d=gxgrd.Grid.open(grid_file).extent_2d(),
+                        coordinate_system=gxgrd.Grid.open(grid_file).coordinate_system,
                         overwrite=True) as v:
 
     v3d_name = v.file_name
+
+    # use the data grid as the relief surface
+    v.set_plane_relief_surface(grid_file)
 
     # add the grid image to the view, with shading, 20 nT contour interval to match default contour lines
     gxgroup.Aggregate_group.new(v, gxagg.Aggregate_image.new(grid_file, shade=True, contour=20))
 
     # contour the grid
     gxgroup.contour(v, 'TMI_contour', grid_file)
+
 
 # display the map in a Geosoft viewer
 gxviewer.view_document(v3d_name, wait_for_close=False)
---
+++ tutorial/3D Views/tmi_on_3d_dem.py
@@ -0,0 +1,32 @@
+import geosoft.gxpy.gx as gx
+import geosoft.gxpy.view as gxview
+import geosoft.gxpy.group as gxgroup
+import geosoft.gxpy.agg as gxagg
+import geosoft.gxpy.grid as gxgrd
+import geosoft.gxpy.viewer as gxviewer
+
+gxc = gx.GXpy()
+
+grid_file = 'Wittichica Creek Residual Total Field.grd'
+dem_file = 'Wittichica DEM.grd'
+
+# create a 3D view
+with gxview.View_3d.new("TMI drapped on DEM",
+                        area_2d=gxgrd.Grid.open(dem_file).extent_2d(),
+                        coordinate_system=gxgrd.Grid.open(dem_file).coordinate_system,
+                        overwrite=True) as v:
+
+    v3d_name = v.file_name
+
+    # use the DEM as the relief surface
+    v.set_plane_relief_surface(dem_file)
+
+    # add the grid image to the view, 20 nT contour interval to match default contour lines
+    gxgroup.Aggregate_group.new(v, gxagg.Aggregate_image.new(grid_file, shade=False, contour=20))
+
+    # contour the grid
+    gxgroup.contour(v, 'TMI_contour', grid_file)
+
+
+# display the map in a Geosoft viewer
+gxviewer.view_document(v3d_name, wait_for_close=False)
