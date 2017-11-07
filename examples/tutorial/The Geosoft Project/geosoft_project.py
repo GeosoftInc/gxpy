22365105105d85e1b9e486f8319ebf5f3a64b9cd
IanMacLeod <ian.macleod@geosoft.com>
2017-11-07 10:43:53 -0500

    Removing "The Geosoft Project" as it was renamed to "Geosoft Project"
    (cherry picked from commit 45bd1725a3215fc07abc243f309452c826770211)

--- examples/tutorial/The Geosoft Project/geosoft_project.py
+++
@@ -1,6 +0,0 @@
-import geosoft.gxpy.project as gxpj
-
-def rungx():
-    project = gxpj.Geosoft_project()
-    gxpj.user_message("Project name: {}".format(project.name),
-                      "Project user: {}".format(project.gid))
