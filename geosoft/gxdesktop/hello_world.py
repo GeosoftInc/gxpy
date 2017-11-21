ad2e7292b61cb33c994e7ca153c671a578005800
Ian MacLeod <ian.macleod@geosoft.com>
2017-11-21 11:17:32 -0500

    Tuning the python.omn experience, adding Hello World extension test as an option.

---
+++ geosoft/gxdesktop/hello_world.py
@@ -0,0 +1,15 @@
+import geosoft.gxapi as gxapi
+import geosoft.gxpy as gxpy
+
+# a python script must have a rungx(), which is executed by OM when the script is run
+def rungx():
+
+    # get the current gx context
+    gxp = gxpy.gx.GXpy()
+
+    # say hello to the user identified by gxp.gid.
+    gxapi.GXSYS.display_message("GX Python", "Hello {}".format(gxp.gid))
+
+if __name__ == "__main__":
+    gxc = gxpy.gx.GXpy()
+    print('Hello {}'.format(gxc.gid))
