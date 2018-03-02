3c855dff0dd8555123a0bdc0a56b0f1ea7a63f2e
IanMacLeod <ian.macleod@geosoft.com>
2018-03-01 21:47:41 -0500

    pep 8

--- geosoft/__init__.py
+++ geosoft/__init__.py
@@ -1,7 +1,6 @@
 # coding = utf-8
 
 import json
-import warnings
 from os.path import dirname, join
 
 with open(join(dirname(__file__), 'pkg_info.json')) as fp:
@@ -12,18 +11,14 @@ version = __version__
 
 __all__ = ['gxapi', 'gxpy']
 
+
 class GXRuntimeError(RuntimeError):
     """
-    A subclass of `RuntimeError <https://docs.python.org/3/library/exceptions.html#RuntimeError>`_ which is the base class
-    for any runtime type error originating from the Geosoft Python APIS
+    A subclass of `RuntimeError <https://docs.python.org/3/library/exceptions.html#RuntimeError>`_
+    which is the base class for any runtime type error originating from the Geosoft Python APIS
 
     .. versionadded:: 9.1
     """
 
     def __init__(self, message):
         super(RuntimeError, self).__init__(message)
-
-import geosoft.gxapi as gxapi
-
-
-
