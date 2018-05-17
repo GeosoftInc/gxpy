59d67bc053b13b3ec400cf9d157777a5895aa67b
Ian MacLeod <ian.macleod@geosoft.com>
2018-05-17 10:03:36 -0400

    validated stress test for 9.4

--- examples/extra_tests/test_stress_size.py
+++ examples/extra_tests/test_stress_size.py
@@ -1,4 +1,5 @@
 import numpy as np
+import gc
 import unittest
 
 import geosoft
@@ -12,41 +13,54 @@ class Test(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls.gx = gx.GXpy()
+        cls.gx = gx.GXpy(log=print)
+
+    def start(self):
+        self._func = self.id().split('.')[-1]
+        gx.gx().log('\n' + self._func)
 
     def test_vv(self):
+        self.start()
 
-        npdata = np.empty(gxapi.iMAX)
+        max = gxapi.iMAX // 16
+        npdata = np.empty(max)
         with gxvv.GXvv(npdata) as vv:
-            self.assertTrue(vv.length, gxapi.iMAX)
+            self.assertTrue(vv.length, max)
         del npdata
+        gc.collect()
+
         npdata = np.empty(gxapi.iMAX + 1)
         self.assertRaises(gxvv.VVException, gxvv.GXvv, npdata)
         del npdata
+        gc.collect()
 
     def test_va(self):
+        self.start()
 
-        max = gxapi.iMAX // 2
+        max = gxapi.iMAX // 16
+        print('max', max)
         npdata = np.empty(max * 2).reshape((max, 2))
         with gxva.GXva(npdata) as va:
             self.assertTrue(va.length, gxapi.iMAX)
         del npdata
-        npdata = np.empty((max + 1) * 2).reshape((max + 1, 2))
+        gc.collect()
+
+        npdata = np.empty((gxapi.iMAX + 1) * 2).reshape(((gxapi.iMAX + 1), 2))
         self.assertRaises(gxva.VAException, gxva.GXva, npdata)
         del npdata
+        gc.collect()
 
     def test_gdb(self):
+        self.start()
 
+        name = None
+        pagesize = 4096
         try:
-            name = None
-            max_index = gxapi.iMAX
-            with gxdb.Geosoft_gdb.new('new', overwrite=True, comp=gxdb.COMP_NONE, pageSize=64) as gdb:
+            max_index = 65534 * pagesize // 8
+            print('maximum index', max_index)
+            with gxdb.Geosoft_gdb.new('new', overwrite=True, comp=gxdb.COMP_NONE, page_size=pagesize) as gdb:
                 name = gdb.file_name
                 line = gdb.new_line('test')
-
-                npd = np.empty(max_index + 1)
-                self.assertRaises(gxvv.VVException, gdb.write_line, line, npd, ['xx'])
-                del npd
 
                 npd = np.zeros(max_index)
                 npd_size = np.size(npd)
