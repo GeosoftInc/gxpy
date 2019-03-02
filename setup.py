00e6d0ca73efc7b9777fe732d9178b42268575e1
Jacques Beaurain <jacques.beaurain@geosoft.com>
2019-03-02 15:44:19 -0500

    Remove geosoft.key and any dist files from module. Can now be fully configured via GXContext, with same default Core key behaviour

--- setup.py
+++ setup.py
@@ -70,103 +70,11 @@ packages=[
 
 package_data={
     'geosoft': ['*.json'],
-    'geosoft.gxapi': ['geosoft.key', 'gxapi_cy.pyd',  'gxapi_cy_extend.pyd', '*.dll'],
+    'geosoft.gxapi': ['gxapi_cy.pyd',  'gxapi_cy_extend.pyd', '*.dll'],
     'geosoft.gxpy._jdcal': ['*.txt', '*.rst'],
     'geosoft.gxpy._xmltodict': ['LICENSE', '*.md'],
     'geosoft.gxpy.user_input': ['*.gx']
 }
-
-redist_packages=[
-    'geosoft.gxapi.GeosoftRedist',
-    'geosoft.gxapi.GeosoftRedist.bin',
-    'geosoft.gxapi.GeosoftRedist.csv',
-    'geosoft.gxapi.GeosoftRedist.etc',
-    'geosoft.gxapi.GeosoftRedist.fonts',
-    'geosoft.gxapi.GeosoftRedist.ger',
-    'geosoft.gxapi.GeosoftRedist.ini',
-    'geosoft.gxapi.GeosoftRedist.tbl',
-    'geosoft.gxapi.GeosoftRedist.tbl.Geophysics',
-    'geosoft.gxapi.GeosoftRedist.tbl.miscellaneous',
-    'geosoft.gxapi.GeosoftRedist.tbl.monochromatic',
-    'geosoft.gxapi.GeosoftRedist.tbl.topography'
-]
-
-redist_package_data={
-    'geosoft.gxapi.GeosoftRedist': ['*'],
-    'geosoft.gxapi.GeosoftRedist.bin': ['*'],
-    'geosoft.gxapi.GeosoftRedist.csv': ['*'],
-    'geosoft.gxapi.GeosoftRedist.etc': ['*'],
-    'geosoft.gxapi.GeosoftRedist.fonts': ['*'],
-    'geosoft.gxapi.GeosoftRedist.ger': ['*'],
-    'geosoft.gxapi.GeosoftRedist.ini': ['*'],
-    'geosoft.gxapi.GeosoftRedist.tbl': ['*'],
-    'geosoft.gxapi.GeosoftRedist.tbl.Geophysics': ['*'],
-    'geosoft.gxapi.GeosoftRedist.tbl.miscellaneous': ['*'],
-    'geosoft.gxapi.GeosoftRedist.tbl.monochromatic': ['*'],
-    'geosoft.gxapi.GeosoftRedist.tbl.topography': ['*']
-}
-
-standalone_dist = None
-standalone_temp = None
-standalone_user = None
-for arg in sys.argv:
-    if arg.startswith('--standalone_dist='):
-        standalone_dist = arg[18:]
-        index = sys.argv.index(arg)
-        sys.argv.pop(index)  # Removes the arg
-
-if standalone_dist:
-    for arg in sys.argv:
-        if arg.startswith('--standalone_temp='):
-            standalone_temp = arg[18:]
-            index = sys.argv.index(arg)
-            sys.argv.pop(index)  # Removes the arg
-    for arg in sys.argv:
-        if arg.startswith('--standalone_user='):
-            standalone_user = arg[18:]
-            index = sys.argv.index(arg)
-            sys.argv.pop(index)  # Removes the arg
-    if not standalone_temp or not standalone_user:
-        raise RuntimeError("Need to specify standalone_temp as well as standalone_user directories with standalone_dist.")
-    packages.extend(redist_packages)
-    package_data = {**package_data, **redist_package_data}
-
-if standalone_dist:
-    if path.exists('geosoft/gxapi/GeosoftRedist'):
-        shutil.rmtree('geosoft/gxapi/GeosoftRedist')
-    zip_ref = zipfile.ZipFile(standalone_dist, 'r')
-    zip_ref.extractall('geosoft/gxapi')
-    zip_ref.close()
-
-key_file = path.join('geosoft', 'gxapi', 'geosoft.key')
-if standalone_dist:
-    if path.exists(key_file):
-        remove(key_file)
-    remove('geosoft/gxapi/geoengine.core.gx_ansi.dll')
-    remove('geosoft/gxapi/geoengine.core.gx_u.dll')
-    remove('geosoft/gxapi/geoengine.core.gxnet.dll')
-    remove('geosoft/gxapi/geoengine.core.gxnetx.dll')
-    remove('geosoft/gxapi/geogx.dll')
-    remove('geosoft/gxapi/geogx_u.dll')
-    remove('geosoft/gxapi/geosoft.desktop.gxnet.dll')
-    remove('geosoft/gxapi/geosoft.desktop.gxnetx.dll')
-    with open('geosoft/gxapi/GeosoftRedist/geosoft.redist', 'w') as f:
-        f.writelines([standalone_user, '\n', standalone_temp, '\n'])
-
-else:
-    with open(key_file, 'w') as f:
-        use_default_key = True
-        for arg in sys.argv:
-            if arg.startswith('--use_geosoft_key='):
-                index = sys.argv.index(arg)
-                sys.argv.pop(index)  # Removes the arg
-                geosoft_key = arg[18:]
-                f.write(geosoft_key)
-                use_default_key = False
-                break
-        if use_default_key:
-            f.write("Core")
-
 
 setup(
     name='geosoft',
